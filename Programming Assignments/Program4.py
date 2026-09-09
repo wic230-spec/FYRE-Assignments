69ee478b5c320c757cae0de50ea88f09bdbe3c7a
William Cariveau <wic230@lehigh.edu>
2026-09-09 15:52:54 -0400

    Enhance Program4.py with comments and structure
    Added comments and improved code readability.

--- Programming Assignments/Program4.py
+++ Programming Assignments/Program4.py
@@ -1,1 +1,15 @@
+# This program was created in Arduino Lab for MicroPython
+#incorperate modules to use functions
+import machine as m
+import time 
 
+#creates object
+#the green lights pin is 0
+led = m.Pin(0, m.Pin.OUT)
+
+#creates an infinite loop
+while (True):
+  led.value(1)
+  time.sleep(0.25)
+  led.value(0)
+  time.sleep(0.25)
