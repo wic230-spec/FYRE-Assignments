694c3126de33de041030f1098d7921551248c9a0
William Cariveau <wic230@lehigh.edu>
2026-09-09 15:10:22 -0400

    Update Program2.py to use variables for names
    Revised the program to use variables for names.

--- Programming Assignments/Program2.py
+++ Programming Assignments/Program2.py
@@ -1,3 +1,8 @@
 # This program was created in Arduino Lab for MicroPython
 #Output to the REPL hello world
 print("Hello World!")
+
+#revised using variables
+name1 = "Will"
+name2 = "Cariveau"
+print(name1, name2)
