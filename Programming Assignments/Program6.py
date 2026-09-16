4901424c8823294d6de2a0859d5fa5b9001af376
William Cariveau <wic230@lehigh.edu>
2026-09-16 14:28:43 -0400

    Create Program6.py

---
+++ Programming Assignments/Program6.py
@@ -0,0 +1,38 @@
+from machine import Pin, PWM
+import time
+
+# Servo on D3
+servo = PWM("D3", freq=50)
+
+# Button on D7
+button = Pin("D7", Pin.IN, Pin.PULL_UP)
+
+# Start at 0 degrees
+position = 0
+
+def set_servo_angle(angle):
+    # Convert 0-180 degrees to servo duty cycle
+    min_duty = 1000
+    max_duty = 9000
+    duty = min_duty + (angle * (max_duty - min_duty) // 180)
+    servo.duty_u16(duty)
+
+set_servo_angle(0)
+
+while True:
+    # Button pressed
+    if button.value() == 0:
+        # Move 180 degrees
+        if position == 0:
+            position = 180
+        else:
+            position = 0
+
+        set_servo_angle(position)
+
+        # Wait for button release
+        while button.value() == 0:
+            time.sleep(0.01)
+
+        # Small debounce delay
+        time.sleep(0.05)
